017936e143e93e0b376d8df4f0e41f5df1d66af5
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-05-14 11:17:28 -0700

    Built the mock api

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
+import { getVideos } from '../mockApi.js';
+
 export default function Video(){
+
+    useEffect(() => {
+    getVideos().then((data) => {
+      console.log("getVideos returned:", data);
+    });
+  }, []);
+
     return(
         <div>
             <h1>Available videos</h1>
             <p>Video list will go here.</p>
         </div>
-    )
+    );
 }
